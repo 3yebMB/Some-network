4eeee0cc6f63c3a018ac7010ee46d64c52081b75
Michael <3yebmb@gmail.com>
2024-09-04 21:51:39 +0200

    Add detecting of different invalid email formats

--- app/src/test/java/dev/m13d/somenet/signup/CredentialsValidationTest.kt
+++ app/src/test/java/dev/m13d/somenet/signup/CredentialsValidationTest.kt
@@ -4,11 +4,22 @@ import dev.m13d.somenet.InstantTaskExecutorExtension
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
 
 @ExtendWith(InstantTaskExecutorExtension::class)
 class CredentialsValidationTest {
 
-    @Test
+    @ParameterizedTest
+    @CsvSource(
+        "''",
+        "' '",
+        "'  '",
+        "'a@b.c'",
+        "'ab@b.c'",
+        "'ab@bc.c'",
+        "'email'",
+    )
     fun invalidEmail() {
         val viewModel = SignUpViewModel()
         viewModel.createAccount("email", ":password:", "about")

06e6d367ba8c862743469454cde39d54bd6994f2
Michael <3yebmb@gmail.com>
2024-09-04 20:10:38 +0200

    Setup JUnit5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,6 +60,12 @@ android {
     buildFeatures {
         buildConfig = true
     }
+    tasks.withType<Test> {
+        useJUnitPlatform()
+        testLogging {
+            events("passed", "failed", "skipped", "standardOut", "standardError")
+        }
+    }
     packaging {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
@@ -85,11 +91,17 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
-    testImplementation(libs.junit)
+
+    testRuntimeOnly(libs.junit.jupiter.engine)
+
+    testImplementation(libs.junit.jupiter.api)
+    testImplementation(libs.junit.jupiter.params)
+
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.ui.test.junit4)
+
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
 }

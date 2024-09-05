d9685f03ad595f4318408ab789282d09c5635f1b
Michael <3yebmb@gmail.com>
2024-09-05 18:27:44 +0200

    Add jetpack compose navigation dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -86,6 +86,7 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
+    implementation(libs.androidx.navigation.compose)
     implementation(libs.androidx.runtime.livedata)
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.ui)

b22c5e3bdd4c7766bc4579c20d27a912978d7d97
Michael <3yebmb@gmail.com>
2024-09-10 23:11:50 +0200

    Extract InMemoryPostsCatalog class into standalone file

--- app/src/main/java/dev/m13d/somenet/timeline/TimelineViewModel.kt
+++ app/src/main/java/dev/m13d/somenet/timeline/TimelineViewModel.kt
@@ -3,7 +3,7 @@ package dev.m13d.somenet.timeline
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import dev.m13d.somenet.domain.post.Post
+import dev.m13d.somenet.domain.post.InMemoryPostsCatalog
 import dev.m13d.somenet.domain.user.Following
 import dev.m13d.somenet.timeline.states.TimelineState
 
@@ -19,18 +19,8 @@ class TimelineViewModel : ViewModel() {
         val userIds = listOf(userId) + followings
             .filter { it.userId == userId }
             .map { it.followedId }
-        val postsForUser = postsFor(userIds)
+        val postsForUser = InMemoryPostsCatalog().postsFor(userIds)
         _timelineState.value = TimelineState.Posts(postsForUser)
     }
-
-    private fun postsFor(userIds: List<String>): List<Post> {
-        val availablePosts = listOf(
-            Post("post2", "lucyId", "post 2", 2L),
-            Post("post1", "lucyId", "post 1", 1L),
-            Post("postId", "timId", "post text", 1L),
-            Post("post4", "sarahId", "post 4", 4L),
-            Post("post3", "sarahId", "post 3", 3L),
-        )
-        return  availablePosts.filter { userIds.contains(it.userId) }
     }
 }

b2916d548e889824025dca2c1e040c2eb24623b8
Michael <3yebmb@gmail.com>
2024-09-10 22:04:26 +0200

    Extract function to get posts by ids

--- app/src/main/java/dev/m13d/somenet/timeline/TimelineViewModel.kt
+++ app/src/main/java/dev/m13d/somenet/timeline/TimelineViewModel.kt
@@ -12,13 +12,6 @@ class TimelineViewModel : ViewModel() {
     val timelineState: LiveData<TimelineState> = _timelineState
 
     fun timelineFor(userId: String) {
-        val availablePosts = listOf(
-            Post("post2", "lucyId", "post 2", 2L),
-            Post("post1", "lucyId", "post 1", 1L),
-            Post("postId", "timId", "post text", 1L),
-            Post("post4", "sarahId", "post 4", 4L),
-            Post("post3", "sarahId", "post 3", 3L),
-        )
         val followings = listOf(
             Following("sarahId", "lucyId"),
             Following("anabelId", "lucyId"),
@@ -26,8 +19,18 @@ class TimelineViewModel : ViewModel() {
         val userIds = listOf(userId) + followings
             .filter { it.userId == userId }
             .map { it.followedId }
-        val postsForSarah = availablePosts.filter { userIds.contains(it.userId) }
-        _timelineState.value = TimelineState.Posts(postsForSarah)
+        val postsForUser = postsFor(userIds)
+        _timelineState.value = TimelineState.Posts(postsForUser)
     }
 
+    private fun postsFor(userIds: List<String>): List<Post> {
+        val availablePosts = listOf(
+            Post("post2", "lucyId", "post 2", 2L),
+            Post("post1", "lucyId", "post 1", 1L),
+            Post("postId", "timId", "post text", 1L),
+            Post("post4", "sarahId", "post 4", 4L),
+            Post("post3", "sarahId", "post 3", 3L),
+        )
+        return  availablePosts.filter { userIds.contains(it.userId) }
+    }
 }

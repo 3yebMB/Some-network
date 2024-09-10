160e28bbe0f894e43a51e4a2a7dd1b4c405497ce
Michael <3yebmb@gmail.com>
2024-09-10 18:24:17 +0200

    Add UserBuilder to help creating users

---
+++ app/src/test/java/dev/m13d/somenet/infrastructure/builder/UserBuilder.kt
@@ -0,0 +1,26 @@
+package dev.m13d.somenet.infrastructure.builder
+
+import dev.m13d.somenet.domain.user.User
+import kotlin.uuid.ExperimentalUuidApi
+import kotlin.uuid.Uuid
+
+@ExperimentalUuidApi
+class UserBuilder {
+    private var userId = Uuid.random().toString()
+
+    private var userEmail = "user@somenet.dev"
+    private var aboutUser = "About User ($userId)"
+    companion object {
+        fun aUser(): UserBuilder {
+            return UserBuilder()
+        }
+    }
+
+    fun withId(id: String): UserBuilder = this.apply {
+        userId = id
+    }
+
+    fun build(): User {
+        return User(userId, userEmail, aboutUser)
+    }
+}
--- app/src/test/java/dev/m13d/somenet/timeline/LoadPostsTest.kt
+++ app/src/test/java/dev/m13d/somenet/timeline/LoadPostsTest.kt
@@ -2,11 +2,13 @@ package dev.m13d.somenet.timeline
 
 import dev.m13d.somenet.InstantTaskExecutorExtension
 import dev.m13d.somenet.domain.post.Post
+import dev.m13d.somenet.infrastructure.builder.UserBuilder.Companion.aUser
 import dev.m13d.somenet.timeline.states.TimelineState
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import kotlin.test.assertEquals
+import kotlin.uuid.ExperimentalUuidApi
 
 @ExperimentalCoroutinesApi
 @ExtendWith(InstantTaskExecutorExtension::class)
@@ -17,5 +19,15 @@ class LoadPostsTest {
         val viewModel = TimelineViewModel()
         viewModel.timelineFor("annaId")
         assertEquals(TimelineState.Posts(emptyList()), viewModel.timelineState.value)
+    }
+
+    @OptIn(ExperimentalUuidApi::class)
+    @Test
+    fun postsAvailable() {
+        val tim = aUser().withId("timId").build()
+        val timPosts = listOf(Post("postId", tim.userId, "post text", 1L))
+        val viewModel = TimelineViewModel()
+        viewModel.timelineFor(tim.userId)
+        assertEquals(TimelineState.Posts(timPosts), viewModel.timelineState.value)
     }
 }

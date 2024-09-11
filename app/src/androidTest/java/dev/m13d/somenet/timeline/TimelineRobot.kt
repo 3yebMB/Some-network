d1459c35e258ffae2a43e665455c2d6ae83a6094
Michael <3yebmb@gmail.com>
2024-09-11 12:08:10 +0200

    Add UI test for the showing empty timeline

---
+++ app/src/androidTest/java/dev/m13d/somenet/timeline/TimelineRobot.kt
@@ -0,0 +1,42 @@
+package dev.m13d.somenet.timeline
+
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.onNodeWithText
+import dev.m13d.somenet.R
+import dev.m13d.somenet.signup.launchSignUpScreen
+import dev.m13d.somenet.utils.ComposeTestRule
+
+fun launchTimelineFor(
+    email: String,
+    password: String,
+    rule: ComposeTestRule,
+    block: TimelineRobot.() -> Unit,
+): TimelineRobot {
+    launchSignUpScreen(rule) {
+        typeEmail(email)
+        typePassword(password)
+        submit()
+    }
+    return TimelineRobot(rule).apply(block)
+}
+
+class TimelineRobot(
+    private val rule: ComposeTestRule
+) {
+
+    infix fun verify(
+        block: TimelineVerification.() -> Unit,
+    ): TimelineVerification {
+        return TimelineVerification(rule).apply(block)
+    }
+}
+
+class TimelineVerification(
+    private val rule: ComposeTestRule
+) {
+    fun emptyTimelineMessageIsShown() {
+        val emptyTimelineMessage = rule.activity.getString(R.string.emptyTimelineMessage)
+        rule.onNodeWithText(emptyTimelineMessage)
+            .assertIsDisplayed()
+    }
+}
---
+++ app/src/androidTest/java/dev/m13d/somenet/timeline/TimelineScreenTest.kt
@@ -0,0 +1,23 @@
+package dev.m13d.somenet.timeline
+
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import dev.m13d.somenet.MainActivity
+import org.junit.Rule
+import org.junit.Test
+
+class TimelineScreenTest {
+
+    @get:Rule
+    val timelineTestRule = createAndroidComposeRule<MainActivity>()
+
+    @Test
+    fun showEmptyTimelineMessage() {
+        val email = "elizabeth"
+        val password = "p@S\$w0rd="
+        launchTimelineFor(email, password, timelineTestRule) {
+            // No operations
+        } verify {
+            emptyTimelineMessageIsShown()
+        }
+    }
+}

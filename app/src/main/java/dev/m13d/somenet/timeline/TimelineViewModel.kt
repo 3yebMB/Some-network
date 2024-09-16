fec33f6a473e8804d31ef5ad98faa5e907d00c1d
Michael <3yebmb@gmail.com>
2024-09-16 13:40:05 +0200

    Loading the timeline in the background thread

--- app/src/main/java/dev/m13d/somenet/timeline/TimelineViewModel.kt
+++ app/src/main/java/dev/m13d/somenet/timeline/TimelineViewModel.kt
@@ -1,5 +1,6 @@
 package dev.m13d.somenet.timeline
 
+import android.annotation.SuppressLint
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -8,6 +9,7 @@ import dev.m13d.somenet.app.CoroutineDispatchers
 import dev.m13d.somenet.domain.timeline.TimelineRepository
 import dev.m13d.somenet.timeline.states.TimelineState
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class TimelineViewModel(
     private val timelineRepository: TimelineRepository,
@@ -17,11 +19,13 @@ class TimelineViewModel(
     private var _timelineState = MutableLiveData<TimelineState>()
     val timelineState: LiveData<TimelineState> = _timelineState
 
+    @SuppressLint("NullSafeMutableLiveData")
     fun timelineFor(userId: String) {
         viewModelScope.launch {
             _timelineState.value = TimelineState.Loading
-            val result = timelineRepository.getTimelineFor(userId)
-            _timelineState.value = result
+            _timelineState.value = withContext(dispatchers.background) {
+                timelineRepository.getTimelineFor(userId)
+            }
         }
     }
 }

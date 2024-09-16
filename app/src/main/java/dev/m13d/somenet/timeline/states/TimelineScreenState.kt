ca594951118a68ae90ac143684f009161fe5bb31
Michael <3yebmb@gmail.com>
2024-09-16 19:05:59 +0200

    amend! Move the TimelineScreenState into a standalone file
    Move the TimelineScreenState into a standalone file

--- app/src/main/java/dev/m13d/somenet/timeline/states/TimelineScreenState.kt
+++ app/src/main/java/dev/m13d/somenet/timeline/states/TimelineScreenState.kt
@@ -1,8 +1,10 @@
 package dev.m13d.somenet.timeline.states
 
 import androidx.annotation.StringRes
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import dev.m13d.somenet.domain.post.Post
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.delay

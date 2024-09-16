3cbedfd8cf9f10bd8ac3cd926861daebde934b5b
Michael <3yebmb@gmail.com>
2024-09-16 22:48:34 +0200

    Extract getting userId in standalone function

--- app/src/main/java/dev/m13d/somenet/postcomposer/CreatePostViewModel.kt
+++ app/src/main/java/dev/m13d/somenet/postcomposer/CreatePostViewModel.kt
@@ -7,18 +7,20 @@ import dev.m13d.somenet.domain.post.Post
 import dev.m13d.somenet.postcomposer.states.CreatePostState
 
 
-class CreatePostViewModel: ViewModel() {
+class CreatePostViewModel : ViewModel() {
 
     private var _postState = MutableLiveData<CreatePostState>()
     val postState: LiveData<CreatePostState> = _postState
 
     fun createPost(postText: String) {
-        if (postText == "Second post") {
-            val post = Post("anotherPostId", "userId", postText, 2L)
-            _postState.value = CreatePostState.Created(post)
+        val userId = loggedInUserId()
+        val post = if (postText == "Second post") {
+            Post("anotherPostId", userId, postText, 2L)
         } else {
-            val post = Post("postId", "userId", postText, 1L)
-            _postState.value = CreatePostState.Created(post)
+            Post("postId", userId, postText, 1L)
         }
+        _postState.value = CreatePostState.Created(post)
     }
+
+    private fun loggedInUserId() = "userId"
 }

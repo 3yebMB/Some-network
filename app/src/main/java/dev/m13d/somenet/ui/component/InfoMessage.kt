43ab32a94a10e4adf4f9f733b698c4b3d7ea203e
Michael <3yebmb@gmail.com>
2024-09-30 14:00:39 +0200

    Remove unnecessary parameter

--- app/src/main/java/dev/m13d/somenet/ui/component/InfoMessage.kt
+++ app/src/main/java/dev/m13d/somenet/ui/component/InfoMessage.kt
@@ -10,21 +10,34 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.delay
 
 @Composable
 fun InfoMessage(
-    isVisible: Boolean,
     @StringRes stringResource: Int,
 ) {
+    if (stringResource == 0) return
+    var shouldShow by remember { mutableStateOf(false) }
+    LaunchedEffect(key1 = "displayMessage") {
+        shouldShow = true
+        delay(1500L)
+        shouldShow = false
+    }
     AnimatedVisibility(
-        visible = isVisible,
+        visible = shouldShow,
         enter = slideInVertically(
             initialOffsetY = { fullHeight -> -fullHeight },
             animationSpec = tween(durationMillis = 150, easing = FastOutLinearInEasing)

cba7e5fff7a7bcc4da7bf8016742c373578c521e
Michael <3yebmb@gmail.com>
2024-10-03 10:57:29 +0200

    Refactor code

--- app/src/main/java/dev/m13d/somenet/friends/FriendsViewModel.kt
+++ app/src/main/java/dev/m13d/somenet/friends/FriendsViewModel.kt
@@ -9,18 +9,23 @@ import dev.m13d.somenet.friends.states.FriendsState
 class FriendsViewModel {
     private val _friendsState = MutableLiveData<FriendsState>()
     val friendsState: LiveData<FriendsState> = _friendsState
+    
     fun loadFriends(userId: String) {
-        if (userId == "jerryId") {
-            val tom = Friend(User("tomId", ":email:", ":about:"), isFollow = false)
-            _friendsState.value = FriendsState.Loaded(listOf(tom))
-        } else if (userId == "lucyId") {
-            val friendAnna = Friend(User("annaId", "", ""), isFollow = true)
-            val friendSara = Friend(User("saraId", "", ""), isFollow = false)
-            val friendTom = Friend(User("tomId", "", ""), isFollow = false)
-            val friends = listOf(friendAnna, friendSara, friendTom)
-            _friendsState.value = FriendsState.Loaded(friends)
-        } else {
-            _friendsState.value = FriendsState.Loaded(emptyList())
+        when (userId) {
+            "jerryId" -> {
+                val tom = Friend(User("tomId", ":email:", ":about:"), isFollow = false)
+                _friendsState.value = FriendsState.Loaded(listOf(tom))
+            }
+            "lucyId" -> {
+                val anna = Friend(User("annaId", "", ""), isFollow = true)
+                val sara = Friend(User("saraId", "", ""), isFollow = false)
+                val tom = Friend(User("tomId", "", ""), isFollow = false)
+                val friends = listOf(anna, sara, tom)
+                _friendsState.value = FriendsState.Loaded(friends)
+            }
+            "samId" -> {
+                _friendsState.value = FriendsState.Loaded(emptyList())
+            }
         }
     }
 }

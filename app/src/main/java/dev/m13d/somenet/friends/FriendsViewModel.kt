0924f0c009bfc7c535a2859050da588d83356f6a
Michael <3yebmb@gmail.com>
2024-10-03 10:04:30 +0200

    Add loading a single friend for hardcoded userId

--- app/src/main/java/dev/m13d/somenet/friends/FriendsViewModel.kt
+++ app/src/main/java/dev/m13d/somenet/friends/FriendsViewModel.kt
@@ -2,12 +2,19 @@ package dev.m13d.somenet.friends
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import dev.m13d.somenet.domain.user.Friend
+import dev.m13d.somenet.domain.user.User
 import dev.m13d.somenet.friends.states.FriendsState
 
 class FriendsViewModel {
     private val _friendsState = MutableLiveData<FriendsState>()
     val friendsState: LiveData<FriendsState> = _friendsState
     fun loadFriends(userId: String) {
-        _friendsState.value = FriendsState.Loaded(emptyList())
+        if (userId == "jerryId") {
+            val tom = Friend(User("tomId", ":email:", ":about:"), isFollow = false)
+            _friendsState.value = FriendsState.Loaded(listOf(tom))
+        } else {
+            _friendsState.value = FriendsState.Loaded(emptyList())
+        }
     }
 }
--- app/src/test/java/dev/m13d/somenet/friends/LoadFriendsTest.kt
+++ app/src/test/java/dev/m13d/somenet/friends/LoadFriendsTest.kt
@@ -1,12 +1,17 @@
 package dev.m13d.somenet.friends
 
 import dev.m13d.somenet.InstantTaskExecutorExtension
+import dev.m13d.somenet.domain.user.Friend
+import dev.m13d.somenet.domain.user.User
 import dev.m13d.somenet.friends.states.FriendsState
+import dev.m13d.somenet.infrastructure.builder.UserBuilder.Companion.aUser
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import kotlin.test.assertEquals
+import kotlin.uuid.ExperimentalUuidApi
 
+@ExperimentalUuidApi
 @ExperimentalCoroutinesApi
 @ExtendWith(InstantTaskExecutorExtension::class)
 class LoadFriendsTest {
@@ -18,5 +23,16 @@ class LoadFriendsTest {
         viewModel.loadFriends("samId")
 
         assertEquals(FriendsState.Loaded(emptyList()), viewModel.friendsState.value)
+    }
+
+    @Test
+    fun loadedSinglePerson() {
+        val user = User("tomId", ":email:", ":about:")
+        val tom = Friend(user, isFollow = false)
+        val viewModel = FriendsViewModel()
+
+        viewModel.loadFriends("jerryId")
+
+        assertEquals(FriendsState.Loaded(listOf(tom)), viewModel.friendsState.value)
     }
 }

77d853104b3919527e4b9bef99ab3aa24f9e3cfe
Michael <3yebmb@gmail.com>
2024-10-03 13:44:48 +0200

    Extract getting friends for particular user into InMemoryFriendsCatalog class

--- app/src/main/java/dev/m13d/somenet/friends/FriendsViewModel.kt
+++ app/src/main/java/dev/m13d/somenet/friends/FriendsViewModel.kt
@@ -2,6 +2,8 @@ package dev.m13d.somenet.friends
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import dev.m13d.somenet.domain.exceptions.BackendException
+import dev.m13d.somenet.domain.exceptions.ConnectionUnavailableException
 import dev.m13d.somenet.domain.user.Friend
 import dev.m13d.somenet.domain.user.User
 import dev.m13d.somenet.friends.states.FriendsState
@@ -11,30 +13,33 @@ class FriendsViewModel {
     val friendsState: LiveData<FriendsState> = _friendsState
 
     fun loadFriends(userId: String) {
-        val friendsState = when (userId) {
-            "jerryId" -> {
-                val tom = Friend(User("tomId", ":email:", ":about:"), isFollow = false)
-                FriendsState.Loaded(listOf(tom))
-            }
-            "lucyId" -> {
-                val anna = Friend(User("annaId", "", ""), isFollow = true)
-                val sara = Friend(User("saraId", "", ""), isFollow = false)
-                val tom = Friend(User("tomId", "", ""), isFollow = false)
-                val friends = listOf(anna, sara, tom)
-                FriendsState.Loaded(friends)
-            }
-            "samId" -> {
-                FriendsState.Loaded(emptyList())
-            }
-            "mihalyId" -> {
-                FriendsState.BackendError
-            }
-            "jovId" -> {
-                FriendsState.Offline
-            }
-
-            else -> { TODO() }
+        val friendsState = try {
+            val friendsForUserId = InMemoryFriendsCatalog().loadFriendsFor(userId)
+            FriendsState.Loaded(friendsForUserId)
+        } catch (backendException: BackendException) {
+            FriendsState.BackendError
+        } catch (offlineException: ConnectionUnavailableException) {
+            FriendsState.Offline
         }
         _friendsState.value = friendsState
+    }
+
+    class InMemoryFriendsCatalog {
+        private val tom = Friend(User("tomId", ":email:", ":about:"), isFollow = false)
+        private val anna = Friend(User("annaId", "", ""), isFollow = true)
+        private val sara = Friend(User("saraId", "", ""), isFollow = false)
+        private val friendsForUserId = mapOf(
+            "jerryId" to listOf(tom),
+            "lucyId" to listOf(anna, sara, tom),
+            "samId" to emptyList(),
+        )
+
+        fun loadFriendsFor(userId: String): List<Friend> {
+            when(userId) {
+                "mihalyId" -> throw BackendException()
+                "jovId" -> throw ConnectionUnavailableException()
+            }
+            return friendsForUserId.getValue(userId)
+        }
     }
 }

2a6f0a1f987efbd66c403c7649a4a84e94f41e63
Michael <3yebmb@gmail.com>
2024-10-03 17:52:29 +0200

    Improve the intention of the test

--- app/src/test/java/dev/m13d/somenet/friends/FailFriendsLoadingTest.kt
+++ app/src/test/java/dev/m13d/somenet/friends/FailFriendsLoadingTest.kt
@@ -1,9 +1,13 @@
 package dev.m13d.somenet.friends
 
 import dev.m13d.somenet.InstantTaskExecutorExtension
+import dev.m13d.somenet.domain.exceptions.BackendException
+import dev.m13d.somenet.domain.exceptions.ConnectionUnavailableException
+import dev.m13d.somenet.domain.friends.FriendsCatalog
 import dev.m13d.somenet.domain.friends.FriendsRepository
 import dev.m13d.somenet.domain.friends.InMemoryFriendsCatalog
 import dev.m13d.somenet.domain.user.Friend
+import dev.m13d.somenet.domain.user.UnavailableUserCatalog
 import dev.m13d.somenet.domain.user.User
 import dev.m13d.somenet.friends.states.FriendsState
 import kotlinx.coroutines.ExperimentalCoroutinesApi
@@ -28,19 +32,33 @@ class FailFriendsLoadingTest {
 
     @Test
     fun backendError() {
-        val viewModel = FriendsViewModel(FriendsRepository(friendsCatalog))
+        val viewModel = FriendsViewModel(FriendsRepository(UnavailableFriendsCatalog()))
 
-        viewModel.loadFriends("mihalyId")
+        viewModel.loadFriends(":irrelevant:")
 
         assertEquals(FriendsState.BackendError, viewModel.friendsState.value)
     }
 
     @Test
     fun offlineError() {
-        val viewModel = FriendsViewModel(FriendsRepository(friendsCatalog))
+        val viewModel = FriendsViewModel(FriendsRepository(OfflineFriendsCatalog()))
 
-        viewModel.loadFriends("jovId")
+        viewModel.loadFriends(":irrelevant:")
 
         assertEquals(FriendsState.Offline, viewModel.friendsState.value)
+    }
+
+    private class UnavailableFriendsCatalog : FriendsCatalog {
+
+        override fun loadFriendsFor(userId: String): List<Friend> {
+            throw BackendException()
+        }
+    }
+
+    private class OfflineFriendsCatalog : FriendsCatalog {
+
+        override fun loadFriendsFor(userId: String): List<Friend> {
+            throw ConnectionUnavailableException()
+        }
     }
 }

155f07ff6266cd9e138270778ad9a93b1845d323
Michael <3yebmb@gmail.com>
2024-10-07 13:49:44 +0200

    Remove the modifier from the FriendsList composable

--- app/src/main/java/dev/m13d/somenet/friends/FriendsScreen.kt
+++ app/src/main/java/dev/m13d/somenet/friends/FriendsScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
@@ -13,6 +14,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import dev.m13d.somenet.R
 import dev.m13d.somenet.domain.user.Friend
@@ -47,12 +49,15 @@ fun FriendsScreen(userId: String) {
 @Composable
 private fun FriendsList(
     friends: List<Friend>,
-    modifier: Modifier = Modifier,
 ) {
     if (friends.isEmpty()) {
-        Text(text = stringResource(R.string.emptyFriendsMessage))
+        Text(
+            text = stringResource(R.string.emptyFriendsMessage),
+            modifier = Modifier.fillMaxWidth(),
+            textAlign = TextAlign.Center,
+        )
     } else {
-        LazyColumn {
+        LazyColumn(modifier = Modifier.fillMaxSize()) {
             items(friends) { friend ->
                 Text(text = friend.user.id)
             }

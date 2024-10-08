034e283e9a0c24282cec39f2fde70186c5f310bb
Michael <3yebmb@gmail.com>
2024-10-08 15:56:17 +0200

    Replace data with abstractions

--- app/src/main/java/dev/m13d/somenet/friends/FriendsViewModel.kt
+++ app/src/main/java/dev/m13d/somenet/friends/FriendsViewModel.kt
@@ -1,15 +1,12 @@
 package dev.m13d.somenet.friends
 
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dev.m13d.somenet.R
 import dev.m13d.somenet.app.CoroutineDispatchers
 import dev.m13d.somenet.domain.friends.FriendsRepository
-import dev.m13d.somenet.domain.user.Friend
-import dev.m13d.somenet.domain.user.User
 import dev.m13d.somenet.friends.states.FriendsScreenState
 import dev.m13d.somenet.friends.states.FriendsState
 import kotlinx.coroutines.launch
@@ -38,20 +35,28 @@ class FriendsViewModel(
     }
 
     fun toggleFollowing(userId: String, followeeId: String) {
-        val user = User(followeeId, "user@somenet.dev", "About User")
-        val newState = FriendsScreenState(friends = listOf(Friend(user, isFollowee = true)))
-        savedStateHandle[SCREEN_STATE_KEY] = newState
+        val currentState = savedStateHandle[SCREEN_STATE_KEY] ?: FriendsScreenState()
+        val index = currentState.friends.indexOfFirst { it.user.id == followeeId }
+        val matchingUser = currentState.friends[index]
+        val updatedFriends = currentState.friends.toMutableList().apply {
+            set(index, matchingUser.copy(isFollowee = true))
+        }
+        val updatedState = currentState.copy(friends = updatedFriends)
+        savedStateHandle[SCREEN_STATE_KEY] = updatedState
     }
 
     private fun updateScreenState(friendsState: FriendsState) {
         val currentState = savedStateHandle[SCREEN_STATE_KEY] ?: FriendsScreenState()
-        val newState = when(friendsState) {
+        val newState = when (friendsState) {
             is FriendsState.Loading ->
                 currentState.copy(isLoading = true)
+
             is FriendsState.Loaded ->
                 currentState.copy(isLoading = false, friends = friendsState.friends)
+
             is FriendsState.BackendError ->
                 currentState.copy(isLoading = false, error = R.string.fetchingFriendsError)
+
             is FriendsState.Offline ->
                 currentState.copy(isLoading = false, error = R.string.offlineError)
         }

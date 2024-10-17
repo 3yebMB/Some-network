a93703b22841033189e08389b16b71856b7d5020
Michael <3yebmb@gmail.com>
2024-10-17 15:59:48 +0200

    Add SavedStateHandle for the TimelineViewModel

--- app/src/main/java/dev/m13d/somenet/timeline/TimelineViewModel.kt
+++ app/src/main/java/dev/m13d/somenet/timeline/TimelineViewModel.kt
@@ -3,10 +3,12 @@ package dev.m13d.somenet.timeline
 import android.annotation.SuppressLint
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dev.m13d.somenet.app.CoroutineDispatchers
 import dev.m13d.somenet.domain.timeline.TimelineRepository
+import dev.m13d.somenet.timeline.states.TimelineScreenState
 import dev.m13d.somenet.timeline.states.TimelineState
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
@@ -16,8 +18,15 @@ class TimelineViewModel(
     private val dispatchers: CoroutineDispatchers,
 ) : ViewModel() {
 
+    private companion object {
+        private const val SCREEN_STATE_KEY = "timelineScreenState"
+    }
+
     private var _timelineState = MutableLiveData<TimelineState>()
     val timelineState: LiveData<TimelineState> = _timelineState
+
+    private val savedStateHandle = SavedStateHandle()
+    val timelineScreenState: LiveData<TimelineScreenState> = savedStateHandle.getLiveData(SCREEN_STATE_KEY)
 
     @SuppressLint("NullSafeMutableLiveData")
     fun timelineFor(userId: String) {

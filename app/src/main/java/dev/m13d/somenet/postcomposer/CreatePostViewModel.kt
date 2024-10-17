ffea2faa653fe661c6587c2e45f831eb6e18572e
Michael <3yebmb@gmail.com>
2024-10-17 23:04:28 +0200

    Add saved state handle

--- app/src/main/java/dev/m13d/somenet/postcomposer/CreatePostViewModel.kt
+++ app/src/main/java/dev/m13d/somenet/postcomposer/CreatePostViewModel.kt
@@ -2,10 +2,12 @@ package dev.m13d.somenet.postcomposer
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dev.m13d.somenet.app.CoroutineDispatchers
 import dev.m13d.somenet.domain.post.PostRepository
+import dev.m13d.somenet.postcomposer.states.CreateNewPostScreenState
 import dev.m13d.somenet.postcomposer.states.CreatePostState
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
@@ -15,8 +17,15 @@ class CreatePostViewModel(
     private val dispatchers: CoroutineDispatchers,
 ) : ViewModel() {
 
+    private companion object {
+        private const val SCREEN_STATE_KEY = "createPostScreenState"
+    }
+
     private var _postState = MutableLiveData<CreatePostState>()
     val postState: LiveData<CreatePostState> = _postState
+
+    private val savedStateHandle = SavedStateHandle()
+    val postScreenState: LiveData<CreateNewPostScreenState> = savedStateHandle.getLiveData(SCREEN_STATE_KEY)
 
     fun createPost(postText: String) {
         viewModelScope.launch {

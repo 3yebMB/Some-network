148494e1b1f646f61def09343a8a02ffaa2c7d2d
Michael <3yebmb@gmail.com>
2024-10-22 00:33:44 +0200

    Remove unused imports

--- app/src/main/java/dev/m13d/somenet/postcomposer/CreatePostViewModel.kt
+++ app/src/main/java/dev/m13d/somenet/postcomposer/CreatePostViewModel.kt
@@ -2,7 +2,6 @@ package dev.m13d.somenet.postcomposer
 
 import androidx.annotation.StringRes
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
--- app/src/main/java/dev/m13d/somenet/timeline/states/TimelineScreenState.kt
+++ app/src/main/java/dev/m13d/somenet/timeline/states/TimelineScreenState.kt
@@ -2,14 +2,7 @@ package dev.m13d.somenet.timeline.states
 
 import android.os.Parcelable
 import androidx.annotation.StringRes
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.setValue
 import dev.m13d.somenet.domain.post.Post
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
